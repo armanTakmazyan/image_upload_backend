ed099558ef1cb477855dbe21d39fbd0eff55e13d
Arman Takmazyan <armanworkmail01@gmail.com>
2021-05-30 18:49:29 +0400

    Changed the routes.js

--- components/images/routes.js
+++ components/images/routes.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '/../uploads/tmp'));
+    cb(null, path.join(__dirname, '/../../uploads/tmp'));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
